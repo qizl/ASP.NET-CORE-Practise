1f30ba0df89c6b2a1654297e0b276b563263900d
qizl <q@qizl.cn>
2017-08-20 23:05:10 +0800

    -

---
+++ ConsoleApp1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IUserService _service;
+
+        public HomeController(IUserService service)
+        {
+            this._service = service;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Hello"] = this._service.SayHello();
+            ViewBag.A = "hahaha~";
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ ConsoleApp1/WebApplication1/Services/UserService.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Services
+{
+    public interface IUserService
+    {
+        string SayHello();
+    }
+
+    public class UserService : IUserService
+    {
+        public string SayHello() => "Hello, NET Core!";
+    }
+}
