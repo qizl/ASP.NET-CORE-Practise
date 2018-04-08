74e267413a9a8d48629fa0e195f0b1324eff36e5
qizl <q@qizl.cn>
2018-04-08 18:10:21 +0800

    -

--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
 
@@ -18,11 +19,27 @@ namespace RazorPagesMovie.Pages.Movies
             _context = context;
         }
 
-        public IList<Movie> Movie { get;set; }
+        public IList<Movie> Movie { get; set; }
+        public string CurrentSearchString { get; set; }
+        public SelectList Genres { get; set; }
+        public string CurrentGenre { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string searchString, string movieGenre)
         {
-            Movie = await _context.Movie.ToListAsync();
+            //Movie = await _context.Movie.ToListAsync();
+            this.CurrentSearchString = searchString;
+            this.CurrentGenre = movieGenre;
+
+            var movies = from m in this._context.Movie select m;
+            var genres = from m in this._context.Movie orderby m.Genre select m.Genre;
+
+            if (!string.IsNullOrEmpty(searchString))
+                movies = movies.Where(m => m.Title.Contains(searchString));
+            if (!string.IsNullOrEmpty(movieGenre))
+                movies = movies.Where(m => m.Genre == movieGenre);
+
+            Genres = new SelectList(await genres.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
