4af85691b32ae6808462f085a816c099ce7a55c6
qizl <q@qizl.cn>
2018-09-28 10:27:01 +0800

    add viewcomponent

--- MvcMovie/Controllers/HomeController.cs
+++ MvcMovie/Controllers/HomeController.cs
@@ -33,5 +33,7 @@ namespace MvcMovie.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult ViewComponent() => View();
     }
 }
---
+++ MvcMovie/ViewComponents/CitySummary.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.ViewComponents
+{
+    public class CitySummary : ViewComponent
+    {
+        public CitySummary() { }
+
+        public string Invoke(int cityCount, int peopleCount) => $"{cityCount} cities, {peopleCount} people";
+    }
+}
