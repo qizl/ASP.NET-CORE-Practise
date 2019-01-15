4dfe2eab2620d5f3dc22e2bfec9d75a5b079d1f8
qizhenlu <q@qizl.cn>
2019-01-15 15:54:10 +0800

    -

---
+++ MvcCore/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MvcCore.TagHelpers
+{
+    [HtmlTargetElement("email")]
+    public class EmailTagHelper : TagHelper
+    {
+        // Can be passed via <email mail-to="..." />. 
+        // Pascal case gets translated into lower-kebab-case.
+        public string MailTo { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";    // Replaces <email> with <a> tag
+            
+            output.Attributes.SetAttribute("href", "mailto:" + MailTo);
+            output.Content.SetContent(MailTo);
+        }
+    }
+}
